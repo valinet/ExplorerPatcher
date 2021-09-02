c42bd8c3aff1805330d52e2d5b87ced9df2f820f
Valentin Radu <valentin.radu@valinet.ro>
2021-09-03 00:04:53 +0300

    Support for Microsoft Detours library * Added support for hooking functions using the Microsoft Detours   library; this, in theory, means that the application can now at   least compile just fine on ARM64; although that is the case, it   currently does not support dynamically patching CTray::Init as I   have yet to come up with assembly to do that. Also, it is   uterly untested on ARM64 as I do not have a machine to test on. * Removed x86 support, compilation targets are now only amd64 and   ARM64

--- ExplorerPatcher/dllmain.c
+++ ExplorerPatcher/dllmain.c
@@ -1,9 +1,60 @@
+#ifndef _M_AMD64
+#ifndef _M_ARM64
+#error This application only supports the amd64 or ARM64 architectures. Compilation aborted.
+#endif
+#endif
+#define HOOK_WITH_FUNCHOOK 0
+#define HOOK_WITH_DETOURS 1
+#define HOW_TO_HOOK HOOK_WITH_FUNCHOOK
 #include <initguid.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <Windows.h>
+#if HOW_TO_HOOK == HOOK_WITH_FUNCHOOK
+#ifdef _M_ARM64
+#error Cannot compile for ARM64 using funchook. Change the source to hook with Detours and try again. Compilation aborted.
+#endif
 #include <funchook.h>
 #include <distorm.h>
+#pragma comment(lib, "funchook.lib")
+#pragma comment(lib, "distorm.lib")
+#elif HOW_TO_HOOK == HOOK_WITH_DETOURS
+#include <detours.h>
+#pragma comment(lib, "detours.lib")
+void* funchook_create(void)
+{
+    return 1;
+}
+int funchook_uninstall(
+    void* _this, 
+    int flags
+)
+{
+    return 0;
+}
+int funchook_destroy(void* _this)
+{
+    return 0;
+}
+int funchook_prepare(
+    void* funchook,
+    void** target_func,
+    void* hook_func
+)
+{
+    DetourTransactionBegin();
+    DetourUpdateThread(GetCurrentThread());
+    DetourAttach(target_func, hook_func);
+    return DetourTransactionCommit();
+}
+int funchook_install(
+    void* funchook,
+    int flags
+)
+{
+    return 0;
+}
+#endif
 #include <tlhelp32.h>
 #include <Psapi.h>
 #pragma comment(lib, "Psapi.lib") // required by funchook
@@ -201,7 +252,11 @@ L"</toast>\r\n";
 
 HWND archivehWnd;
 
+#if HOW_TO_HOOK == HOOK_WITH_FUNCHOOK
 funchook_t* funchook = NULL;
+#elif HOW_TO_HOOK == HOOK_WITH_DETOURS
+void* funchook = NULL;
+#endif
 HMODULE hModule = NULL;
 HANDLE hIsWinXShown = NULL;
 
@@ -3271,6 +3326,7 @@ __declspec(dllexport) DWORD WINAPI main(
                 (PDWORD)(&dwNumberOfBytes)
             );
             printf("Copied %d bytes to disassemble.\n", BYTES_TO_DISASSEMBLE);
+#if HOW_TO_HOOK == HOOK_WITH_FUNCHOOK
             _DecodedInst decodedInstructions[1000];
             UINT decodedInstructionsCount = 0;
             _DecodeResult res = distorm_decode(
@@ -3317,25 +3373,6 @@ __declspec(dllexport) DWORD WINAPI main(
                 start = 0;
                 strat++;
             }
-#ifdef DEBUG
-            /*res = distorm_decode(
-                0,
-                (const unsigned char*)m,
-                BYTES_TO_DISASSEMBLE,
-                Decode64Bits,
-                decodedInstructions,
-                1000,
-                &decodedInstructionsCount
-            );
-            for (UINT i = 0; i < decodedInstructionsCount; ++i)
-            {
-                printf(
-                    "0x%p\t%s\t%s\n",
-                    decodedInstructions[i].offset,
-                    decodedInstructions[i].mnemonic.p,
-                    decodedInstructions[i].instructionHex.p
-                );
-            }*/
 #endif
             VirtualProtect(
                 (LPVOID)CTray_Init,
