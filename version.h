02c485e0d96923bb0c652f861787d31dc88d539d
Valentin Radu <valentin.radu@valinet.ro>
2021-11-15 03:46:33 +0200

    Version number is shared between projects

---
+++ version.h
@@ -0,0 +1,16 @@
+#define VER_MAJOR 22000
+#define VER_MINOR 318
+#define VER_BUILD_HI    37
+#define VER_BUILD_LO 2
+#define VER_FLAGS   VS_FF_PRERELEASE
+
+
+// The Binary form of the version numbers
+#define VER_FILE    VER_MAJOR, VER_MINOR, VER_BUILD_HI, VER_BUILD_LO
+#define VER_PRODUCT VER_MAJOR, VER_MINOR, VER_BUILD_HI, VER_BUILD_LO
+
+#define VER_STR(arg) #arg
+
+// The String form of the version numbers
+#define VER_FILE_STRING VALUE "FileVersion", "22000.318.37.2"
+#define VER_PRODUCT_STRING VALUE "ProductVersion", "22000.318.37.2"
