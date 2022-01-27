9d3e34cb89419b023d88e0af9b59ce7700db8f13
Valentin Radu <valentin.radu@valinet.ro>
2022-01-27 23:19:16 +0200

    `ep_setup /extract` extracts all components

--- ep_setup/ep_setup.c
+++ ep_setup/ep_setup.c
@@ -372,6 +372,19 @@ int WINAPI wWinMain(
             wcscat_s(wszPath, MAX_PATH, L"\\ep_weather_host_stub.dll");
             bOk = InstallResource(TRUE, hInstance, IDR_EP_WEATHER_STUB, wszPath);
         }
+        if (argc >= 2)
+        {
+            wcsncpy_s(wszPath, MAX_PATH, wargv[1], MAX_PATH);
+        }
+        else
+        {
+            GetCurrentDirectoryW(MAX_PATH, wszPath);
+        }
+        if (bOk)
+        {
+            wcscat_s(wszPath, MAX_PATH, L"\\WebView2Loader.dll");
+            bOk = InstallResource(TRUE, hInstance, IDR_MS_WEBVIEW2_LOADER, wszPath);
+        }
         return 0;
     }
 
