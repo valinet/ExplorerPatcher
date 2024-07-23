04fd2b7647ebb0b3ed3089d65cf85aa9c2e8a1c8
Amrsatrio <satrionareswara@yahoo.com>
2024-07-24 00:36:01 +0700

    Build: Don't build CMake projects in the vcxproj because it doesn't work with GitHub Actions

--- BuildDependenciesRelease.bat
+++ BuildDependenciesRelease.bat
@@ -8,3 +8,8 @@ cmake libs/funchook -Blibs/funchook/build-x64 -G "Visual Studio 17 2022" -A x64 
 cmake libs/funchook -Blibs/funchook/build-arm64 -G "Visual Studio 17 2022" -A ARM64 -D"CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>" -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DFUNCHOOK_CPU=arm64 -DFUNCHOOK_DISASM=capstone -DFUNCHOOK_BUILD_TESTS=OFF
 cmake libs/zlib -Blibs/zlib/build-x64 -G "Visual Studio 17 2022" -A x64 -D"CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>" -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
 cmake libs/zlib -Blibs/zlib/build-arm64 -G "Visual Studio 17 2022" -A ARM64 -D"CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>" -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
+
+cmake --build libs/funchook/build-x64 --config Release
+cmake --build libs/funchook/build-arm64 --config Release
+cmake --build libs/zlib/build-x64 --config Release
+cmake --build libs/zlib/build-arm64 --config Release
