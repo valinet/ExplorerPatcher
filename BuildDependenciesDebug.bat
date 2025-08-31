ff30457ff54b4b0e397fbafcc098eead2e12b89b
Amrsatrio <satrionareswara@yahoo.com>
2025-08-31 16:49:18 +0700

    Change hooking library to SlimDetours. - On ARM64, fixes incompatibilities with certain Windhawk mods that hook CreateWindowExW such as taskbar-volume-control. - On ARM64, fixes a very slim chance bug where hooked functions would cause a crash when called. - Updated dependencies.

--- BuildDependenciesDebug.bat
+++ BuildDependenciesDebug.bat
@@ -1,6 +1,3 @@
-rmdir /s /q libs\funchook\build
-rmdir /s /q libs\funchook\build-x64
-rmdir /s /q libs\funchook\build-arm64
 rmdir /s /q libs\zlib\build-x64
 rmdir /s /q libs\zlib\build-arm64
 
@@ -10,12 +7,8 @@ if "%VSINSTALLDIR:~-1%"=="\" (
     set "EP_VSINSTALLDIR=%VSINSTALLDIR%"
 )
 
-cmake libs/funchook -Blibs/funchook/build-x64 -G "Visual Studio 17 2022" -A x64 -D"CMAKE_GENERATOR_INSTANCE:PATH=%EP_VSINSTALLDIR%" -D"CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>" -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DFUNCHOOK_CPU=x86 -DFUNCHOOK_BUILD_TESTS=OFF
-cmake libs/funchook -Blibs/funchook/build-arm64 -G "Visual Studio 17 2022" -A ARM64 -D"CMAKE_GENERATOR_INSTANCE:PATH=%EP_VSINSTALLDIR%" -D"CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>" -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DFUNCHOOK_CPU=arm64 -DFUNCHOOK_DISASM=capstone -DFUNCHOOK_BUILD_TESTS=OFF
 cmake libs/zlib -Blibs/zlib/build-x64 -G "Visual Studio 17 2022" -A x64 -D"CMAKE_GENERATOR_INSTANCE:PATH=%EP_VSINSTALLDIR%" -D"CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>" -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
 cmake libs/zlib -Blibs/zlib/build-arm64 -G "Visual Studio 17 2022" -A ARM64 -D"CMAKE_GENERATOR_INSTANCE:PATH=%EP_VSINSTALLDIR%" -D"CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>" -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
 
-cmake --build libs/funchook/build-x64 --config Debug
-cmake --build libs/funchook/build-arm64 --config Debug
 cmake --build libs/zlib/build-x64 --config Debug
 cmake --build libs/zlib/build-arm64 --config Debug
--- ExplorerPatcher/hooking.h
+++ ExplorerPatcher/hooking.h
@@ -1,80 +1,45 @@
 #ifndef _H_HOOKING_H_
 #define _H_HOOKING_H_
 
-#define STRAT_REPLACE_ANY_TYPE_OF_JUMP_WITH_NOP 0
-#define STRAT_REPLACE_ANY_TYPE_OF_JUMP_WITH_ALWAYS_JUMP 1
-#define HOOK_WITH_FUNCHOOK 0
-#define HOOK_WITH_DETOURS 1
-#define HOW_TO_HOOK HOOK_WITH_FUNCHOOK
-
-#if HOW_TO_HOOK == HOOK_WITH_FUNCHOOK
-
-#include <funchook.h>
-#pragma comment(lib, "funchook.lib")
-#pragma comment(lib, "Psapi.lib") // required by funchook
-#if defined(_M_IX86) || defined(_M_X64)
-#pragma comment(lib, "distorm.lib")
-#else
-#pragma comment(lib, "capstone.lib")
-#endif
-
-#elif HOW_TO_HOOK == HOOK_WITH_DETOURS
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-#include <detours.h>
-#pragma comment(lib, "detours.lib")
+#include <KNSoft/SlimDetours/SlimDetours.h>
 
-#ifdef __cplusplus
-inline
-#endif
-void* funchook_create(void)
+typedef struct funchook funchook_t;
+
+inline funchook_t* funchook_create(void)
 {
-    return 1;
+    return (funchook_t*)1;
 }
 
-#ifdef __cplusplus
-inline
-#endif
-int funchook_uninstall(
-    void* _this,
+inline int funchook_uninstall(
+    funchook_t* _this,
     int flags
 )
 {
     return 0;
 }
 
-#ifdef __cplusplus
-inline
-#endif
-int funchook_destroy(void* _this)
+inline int funchook_destroy(funchook_t* _this)
 {
     return 0;
 }
 
-#ifdef __cplusplus
-inline
-#endif
-int funchook_prepare(
-    void* funchook,
+inline int funchook_prepare(
+    funchook_t* funchook,
     void** target_func,
     void* hook_func
 )
 {
-    DetourTransactionBegin();
-    DetourUpdateThread(GetCurrentThread());
-    DetourAttach(target_func, hook_func);
-    return DetourTransactionCommit();
+    HRESULT hr = SlimDetoursInlineHook(TRUE, target_func, hook_func);
+    return SUCCEEDED(hr) ? 0 : hr;
 }
 
-#ifdef __cplusplus
-inline
-#endif
-int funchook_install(
-    void* funchook,
+inline int funchook_install(
+    funchook_t* funchook,
     int flags
 )
 {
@@ -94,20 +59,8 @@ extern "C"
 {
 #endif
 
-#if HOW_TO_HOOK == HOOK_WITH_FUNCHOOK
-#ifdef __cplusplus
-inline
-#endif
-funchook_t* funchook;
-#elif HOW_TO_HOOK == HOOK_WITH_DETOURS
-#ifdef __cplusplus
-inline
-#endif
-void* funchook;
-#endif
+DECLSPEC_SELECTANY funchook_t* funchook = NULL;
 
 #ifdef __cplusplus
 } // extern "C"
-#endif
-
 #endif
